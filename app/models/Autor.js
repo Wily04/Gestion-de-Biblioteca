d0456c986cbaa97d1d12794119b5026758166e8b
marioborjas <mari_borjas@unicah.edu>
2025-04-07 15:32:32 -0600

    error quitado

--- app/models/Autor.js
+++ app/models/Autor.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { DataTypes } = require('sequelize');
-const { SequelizeInstance } = require('../config/db');
+
 
 module.exports = (sequelize) =>{
  const Autor = sequelize.define('Autor', {
