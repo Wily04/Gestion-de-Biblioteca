a50e731da8ca0109507b79898e9ca983599a7f71
036Carlos <carlosgarcian.2005@gmail.com>
2025-04-08 21:51:49 -0600

    librosRoutes.js

--- app/app.js
+++ app/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const App=express();
-const autorRoutes = require('./Routes/AutorRoutes');
+const cors = require('cors');
+
+const App = express();
+
+const autorRoutes = require('./Routes/autorRoutes');
+const usuarioRoutes = require('./Routes/usuarioRoutes');
+const librosRoutes = require('./Routes/librosRoutes');
 
 App.use(express.json());
 App.use(express.urlencoded({extended:false}));
+App.use(cors());
 
-App.use('/', autorRoutes);
+App.use('/autores', autorRoutes);
+App.use('/usuarios', usuarioRoutes);
+App.use('/libros', librosRoutes);
 
 module.exports=App;
